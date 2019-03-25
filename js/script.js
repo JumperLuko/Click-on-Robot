90dce17620ee53f8e7777c99c22854d20484966e
Jumper Luko <jumper.luko@gmail.com>
2019-03-24 22:03:35 -0300

    Random area work, not very well.

--- js/script.js
+++ js/script.js
@@ -18,21 +18,16 @@ sizeDisplay();
 document.getElementsByTagName("BODY")[0].onresize = function() {sizeDisplay()};
 
 IDboxToRun.addEventListener('mousemove', function(e){
-    cursorX = (e.clientX);
-    cursorY = (e.clientY-50);
+    var cursorX = (e.clientX);
+    var cursorY = (e.clientY-50);
 	IDcursorX.textContent = 'X cursor: ' + cursorX + 'px';
     IDcursorY.textContent = 'Y cursor: ' + cursorY + 'px';
 
-    robotSizeX = IDrobot.clientWidth;
-    robotSizeY = IDrobot.clientHeight;
-    robotX = displayX-cursorX-robotSizeX/2;
-    robotY = displayY-cursorY-robotSizeY/2;
-
-    var startRandomX = displayX/2 - displayX*0.3/2;
-    var endRandomX = displayX/2 + displayX*0.3/2;
-    var startRandomY = displayY/2 - displayY*0.3/2;
-    var endRandomY = displayY/2 + displayY*0.3/2;
-    if((cursorX > startRandomX) == false || (cursorX < endRandomX) == false || (cursorY > startRandomY) == false || (cursorY < endRandomY) == false){
+    var robotSizeX = IDrobot.clientWidth;
+    var robotSizeY = IDrobot.clientHeight;
+    var robotX = displayX-cursorX-robotSizeX/2;
+    var robotY = displayY-cursorY-robotSizeY/2;
+    function robotRun(){
         if(robotX > 0 && robotX < displayX-robotSizeX){//bug in border of the screen
             robot.style.marginLeft = robotX + "px";
             IDrobotX.textContent = 'X robot: ' + robotX + 'px';
@@ -41,7 +36,23 @@ IDboxToRun.addEventListener('mousemove', function(e){
             robot.style.marginTop = robotY + "px"; 
             IDrobotY.textContent = 'Y robot: ' + robotY + 'px';
         }
+    }
+
+    var startRandomX = displayX/2 - displayX*0.3/2;
+    var endRandomX = displayX/2 + displayX*0.3/2;
+    var startRandomY = displayY/2 - displayY*0.3/2;
+    var endRandomY = displayY/2 + displayY*0.3/2;
+    if((cursorX > startRandomX) == false || (cursorX < endRandomX) == false || (cursorY > startRandomY) == false || (cursorY < endRandomY) == false){
+        robotRun();
     }else{
-        alert('Will be random Area, ESC to escape.');
+        // setInterval(function(){
+        //     console.log('Test');
+        // }, 1000)
+        // setInterval(function(){ console.log("Hello"); }, 3000); // Problem with loop only in that condition, if not else STOP! Lack update in some time on loop.
+        var cursorX = Math.floor(Math.random() * (endRandomX+endRandomX - startRandomX-startRandomX/2.5) + startRandomX-startRandomX/2.5);
+        var robotX = displayX-cursorX-robotSizeX/2;
+        var cursorY = Math.floor(Math.random() * (endRandomY+endRandomY - startRandomY-startRandomY) + startRandomY-startRandomY);
+        var robotY = displayY-cursorY-robotSizeY/2;
+        robotRun();
     }
 })
